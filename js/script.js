b60ff54ad659ece3297edb7340848ca4b023670e
BonnotJordan <37403989+BonnotJordan@users.noreply.github.com>
2018-11-02 20:05:32 +0100

    add functions to check if winner

--- js/script.js
+++ js/script.js
@@ -33,26 +33,96 @@ function createTable() {
 
 function checkIfWinner() {
 	console.log("check");
+	// première ligne
+	if(document.getElementById("cell00").innerHTML==document.getElementById("cell01").innerHTML && document.getElementById("cell00").innerHTML==document.getElementById("cell02").innerHTML){
+		if(document.getElementById("cell00").innerHTML == "X"){
+			console.log("Player 1 win 1 line");
+		} else if (document.getElementById("cell00").innerHTML == "O") {
+			console.log("Player 2 win 1 line");
+		}
 	
+	} 
+	// deuxième ligne
+	else if(document.getElementById("cell10").innerHTML==document.getElementById("cell11").innerHTML && document.getElementById("cell10").innerHTML==document.getElementById("cell12").innerHTML){
+		if(document.getElementById("cell10").innerHTML == "X"){
+			console.log("Player 1 win 2 line");
+		} else if(document.getElementById("cell10").innerHTML == "O"){
+			console.log("Player 2 win 2 line");
+		}
+	
+	} 
+	// troisième ligne
+	else if(document.getElementById("cell20").innerHTML==document.getElementById("cell21").innerHTML && document.getElementById("cell20").innerHTML==document.getElementById("cell22").innerHTML){
+		if(document.getElementById("cell20").innerHTML == "X"){
+			console.log("Player 1 win 3 line");
+		} else if (document.getElementById("cell20").innerHTML == "O") {
+			console.log("Player 2 win 3 line");
+		}
+	
+	} 
+	// première colonne
+	else if(document.getElementById("cell00").innerHTML==document.getElementById("cell10").innerHTML && document.getElementById("cell00").innerHTML==document.getElementById("cell20").innerHTML){
+		if(document.getElementById("cell00").innerHTML == "X"){
+			console.log("Player 1 win 1 col");
+		} else if (document.getElementById("cell00").innerHTML == "O") {
+			console.log("Player 2 win 1 col");
+		}
+	
+	} 
+	// deuxième colonne
+	else if(document.getElementById("cell01").innerHTML==document.getElementById("cell11").innerHTML && document.getElementById("cell01").innerHTML==document.getElementById("cell21").innerHTML){
+		if(document.getElementById("cell01").innerHTML == "X"){
+			console.log("Player 1 win 2 col");
+		} else if (document.getElementById("cell01").innerHTML == "O"){
+			console.log("Player 2 win 2 col");
+		}
+	
+	}
+	// troisième colonne 
+	else if(document.getElementById("cell02").innerHTML==document.getElementById("cell12").innerHTML && document.getElementById("cell02").innerHTML==document.getElementById("cell22").innerHTML){
+		if(document.getElementById("cell02").innerHTML == "X"){
+			console.log("Player 1 win 3 col");
+		} else if (document.getElementById("cell02").innerHTML == "O"){
+			console.log("Player 2 win 3 col");
+		}
+	
+	} 
+	// diagonale \
+	else if(document.getElementById("cell00").innerHTML==document.getElementById("cell11").innerHTML && document.getElementById("cell00").innerHTML==document.getElementById("cell22").innerHTML){
+		if(document.getElementById("cell00").innerHTML == "X"){
+			console.log("Player 1 win 1 diag");
+		} else if (document.getElementById("cell00").innerHTML == "O"){
+			console.log("Player 2 win 1 diag");
+		}
+	} 
+	//diagonale /
+	else if(document.getElementById("cell02").innerHTML==document.getElementById("cell11").innerHTML && document.getElementById("cell02").innerHTML==document.getElementById("cell20").innerHTML){
+		if(document.getElementById("cell02").innerHTML == "X"){
+			console.log("Player 1 win 2 diag");
+		} else if (document.getElementById("cell02").innerHTML == "O"){
+			console.log("Player 2 win 2 diag");
+		}
+	}
 }
+
+
 
 var onClickFunction = function()
 {
 	var cellId = this.id;
     var cellClicked = document.getElementById(cellId);
-    var cellClickedValue = document.getElementById(cellId).value;
-    if(cellClickedValue === "X" || cellClickedValue ==="O"){
-    	console.log("Impossible");
+    if(cellClicked.innerHTML == "X" || cellClicked.innerHTML =="O"){
+    	console.log("Impossible sur la case "+cellClicked);
+    } else {
+    	if (lastPlayer == 1){
+    		cellClicked.innerHTML = "X"
+			lastPlayer =2;
+		}
+		else if (lastPlayer == 2){
+			cellClicked.innerHTML = "O"
+			lastPlayer =1;
+		}
     }
-    if (lastPlayer == 1){
-    	cellClicked.innerHTML = "X"
-		lastPlayer =2;
-	}
-	else if (lastPlayer == 2){
-		cellClicked.innerHTML = "O"
-		lastPlayer =1;
-	}
-	
 	var textPlayerTurn = document.getElementById("playerTurn");
 	textPlayerTurn.innerHTML = "";
 	textPlayerTurn.innerHTML += "Au tour du joueur "+lastPlayer;
@@ -71,7 +141,7 @@ function setOnClickMethodToCells() {
 	}
 }
 
-document.getElementById("raz").addEventListener("click",function(){
+function razFunction() {
 	console.log("RAZ clicked");
 	lastPlayer = 1;
 	for(var i=0;i<3;i++){
@@ -79,8 +149,7 @@ document.getElementById("raz").addEventListener("click",function(){
 			document.getElementById("cell"+i+j).innerHTML = "";
 		}
 	}
-});
-
+}
 
 // Function calls
 createTable();
